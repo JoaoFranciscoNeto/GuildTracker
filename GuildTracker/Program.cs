84fce0a9a994182c1024646c3fa5f889cf867b53
João Neto <jneto@mercedesamgf1.com>
2020-11-13 20:41:53 +0000

    Initial project commit

---
+++ GuildTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuildTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
